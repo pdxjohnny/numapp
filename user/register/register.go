cfd33bbc62c1ad52a31c5252c0548f04eef738e2
John Andersen <johnandersenpdx@gmail.com>
2015-10-29 15:41:08 -0700

    More checks for register
    Signed-off-by: John Andersen <johnandersenpdx@gmail.com>

--- user/register/register.go
+++ user/register/register.go
@@ -21,6 +21,7 @@ func Register(registerDoc map[string]interface{}) error {
 	}
 	// Get the properties we need
 	id := registerDoc["username"].(string)
+	email := registerDoc["email"].(string)
 	password := registerDoc["password"].(string)
 	reCAPTCHA := registerDoc["g-recaptcha-response"].(string)
 	// Make sure the user does not exist already
@@ -42,19 +43,26 @@ func Register(registerDoc map[string]interface{}) error {
 	if err != nil {
 		return err
 	}
-	registerDoc["password"] = hashedPassword
 
-	_, err = api.SaveUser(variables.ServiceDBURL, id, registerDoc)
+	// Take only the parts we care about and save them
+	saveDoc := map[string]interface{}{
+		"id":       id,
+		"email":    email,
+		"password": hashedPassword,
+	}
+
+	_, err = api.SaveUser(variables.ServiceDBURL, id, saveDoc)
 	if err != nil {
 		return err
 	}
-	log.Println("User registered", registerDoc)
+	log.Println("User registered", saveDoc)
 	return nil
 }
 
 // checkPropsExist makes sure all the properties that need to exist do
 // before attempting to register
 func checkPropsExist(registerDoc map[string]interface{}) error {
+	// Make sure the user provides a username
 	id, ok := registerDoc["username"].(string)
 	if ok != true {
 		return errors.New("Need username to register")
@@ -62,6 +70,7 @@ func checkPropsExist(registerDoc map[string]interface{}) error {
 		// FIXME: Make concatination constant
 		return errors.New("Username must be at least " + strconv.Itoa(variables.ShortestUsername))
 	}
+	// Make sure the user provides a password
 	password, ok := registerDoc["password"].(string)
 	if ok != true {
 		return errors.New("Need password to register")
@@ -69,6 +78,19 @@ func checkPropsExist(registerDoc map[string]interface{}) error {
 		// FIXME: Make concatination constant
 		return errors.New("Password must be at least " + strconv.Itoa(variables.ShortestPassword))
 	}
+	// Make sure the passwords match
+	confirmPassword, ok := registerDoc["confirm_password"].(string)
+	if ok != true {
+		return errors.New("Need confirm_password to register")
+	} else if password != confirmPassword {
+		return errors.New("Passwords do not match")
+	}
+	// Make sure the user provides an email
+	_, ok = registerDoc["email"].(string)
+	if ok != true {
+		return errors.New("Need email to register")
+	}
+	// Make sure the user provides a recaptcha
 	_, ok = registerDoc["g-recaptcha-response"].(string)
 	if ok != true {
 		return errors.New("Need g-recaptcha-response to register")
